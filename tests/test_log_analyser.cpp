014eda6a85bd3419fec8dc0dc768b858215b49e1
Hedi <hedi@example.com>
2026-04-08 16:49:03 +1000

    Add regex-based parsing for real-world log format

--- tests/test_log_analyser.cpp
+++ tests/test_log_analyser.cpp
@@ -4,9 +4,9 @@
 
 TEST(LogAnalyserTest, CountsCorrectly) {
 	std::ofstream file("test.log");
-	file << "INFO A \n";
-	file << "WARNING B\n";
-	file << "ERROR C\n";
+	file << "2024-01-01 10:00:00 INFO A \n";
+	file << "2024-01-01 10:00:01 WARNING B\n";
+	file << "2024-01-01 10:00:03 ERROR C\n";
 	file.close();
 
 	LogAnalyser analyser;
@@ -37,7 +37,7 @@ TEST(LogAnalyserEdgeCases, EmptyFile){
 
 TEST(LogAnalyserTest, OnlyInfoLogs){
 	std::ofstream file("info.log");
-	file << "INFO A\nINFO B\n";
+	file << "2024-02-02 11:00:00 INFO A\n2024-02-02 11:00:03 INFO B\n";
 	file.close();
 
 	LogAnalyser analyser;
@@ -53,10 +53,10 @@ TEST(LogAnalyserTest, OnlyInfoLogs){
 
 TEST(LogAnalyserTest, MixedLogs){
 	std::ofstream file("mixed.log");
-	file << "INFO Start\n";
-	file << "ERROR Fail\n";
-	file << "WARNING Memory\n";
-	file << "INFO Again\n";
+	file << "2024-05-01 08:00:00 INFO Start\n";
+	file << "2024-05-01 08:00:02 ERROR Fail\n";
+	file << "2024-05-01 08:00:04 WARNING Memory\n";
+	file << "2024-05-01 08:00:06 INFO Again\n";
 	file.close();
 
 	LogAnalyser analyser;
